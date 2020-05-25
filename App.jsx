c542d35b4ce4fbaf66c2e6ba5097bd79d965dddb
phucuongit <noname21062000@gmail.com>
2020-05-25 15:09:36 +0700

    init project

---
+++ App.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import { createAppContainer } from "react-navigation";
+import { createStackNavigator } from "react-navigation-stack";
+import IndexScreen from "./src/screens/IndexScreen";
+
+const navigator = createStackNavigator(
+  {
+    Index: IndexScreen,
+  },
+  {
+    initialRouteName: "Index",
+    defaultNavigationOptions: {
+      title: "Blogs",
+    },
+  }
+);
+
+export default createAppContainer(navigator);
---
+++ src/screens/IndexScreen.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+const IndexScreen = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Index Screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+export default IndexScreen;
